8cf9f2c88b6e1f3237a5a449c580d51a74560ee0
Yawen Guan <Gw980091931@gmail.com>
2020-08-17 16:56:47 +0800

    Update data structures

--- merlion/common.py
+++ merlion/common.py
@@ -9,7 +9,7 @@ class DocstringElementType(enum.Enum):
     Raise = 5, 
     Example = 6,
     Attribute = 7,
-    TODO = 8,
+    Todo = 8,
     Yield = 9,
     Note = 10
 
@@ -25,16 +25,20 @@ class DocstringMeta:
 
 
 class DocstringParam(DocstringMeta):
+    """ Args for function, or Module level variable for module(in the Arrtibute section of the module docstring)
+    """
     def __init__(
         self,
         args: typing.List[str],
         description: typing.Optional[str],
         param_name: str,
-        type_name: typing.Optional[str]
+        type_name: typing.Optional[str],
+        is_module_level_variable: bool
     ) -> None:
         super().__init__(args, description)
         self.param_name = param_name
         self.type_name = type_name
+        self.is_module_level_variable = is_module_level_variable
 
 
 class DocstringReturn(DocstringMeta):
@@ -66,11 +70,25 @@ class DocstringRaise(DocstringMeta):
         self.type_name = type_name
 
 
+class DocstringOther(DocstringMeta):
+    """Summary, Description, Example, Attribute, Todo, Note ...
+    """
+    def __init__(
+        self,
+        args: typing.List[str],
+        description: typing.Optional[str],
+        type: DocstringElementType,
+        long_description: typing.Optional[str]
+    ) -> None:
+        super().__init__(args, description)
+        self.type = type
+        self.long_description = long_description
+
 class DocstringElement:
     def __init__(
         self,
         type: DocstringElementType,
-        element: typing.Union[DocstringParam, DocstringReturn, DocstringRaise]
+        element: typing.Union[DocstringParam, DocstringReturn, DocstringRaise, DocstringOther]
     ) -> None:
         self.type = type,
         self.element = element
@@ -78,4 +96,12 @@ class DocstringElement:
 
 class Docstring:
     def __init__(self) -> None:
-        pass
+        self.elements: typing.List[DocstringElement] = []
+
+    def getSpecificTypeElement(
+        self,
+        type: DocstringElementType
+    ) -> typing.List[DocstringElement]:
+        return [elem for elem in self.elements if elem.type == type]
+
+
